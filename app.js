278570044c1c7e017370dcd67565c23a38dc034f
Mahmoud Helal <mahmoud.helal@vodafone.com>
2019-08-26 01:52:53 +0200

    Linit code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const db = mongoose.connect('mongodb://127.0.0.1:27018/bookAPI');
+
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
@@ -22,7 +23,7 @@ bookRouter.route('/books')
     });
   });
 
-  bookRouter.route('/books/:bookId')
+bookRouter.route('/books/:bookId')
   .get((req, res) => {
     Book.findById(req.params.bookId, (err, books) => {
       if (err) {

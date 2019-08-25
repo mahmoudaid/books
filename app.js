e53c8d2d83d84e98d94638ddda8527c8cda0fff9
Mahmoud Helal <mahmoud.helal@vodafone.com>
2019-08-26 01:41:47 +0200

    init nodejs recap project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const db = mongoose.connect('mongodb://127.0.0.1:27018/bookAPI');
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const query = {};
+    if(req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        res.send(err);
+      } else {
+        return res.json(books);
+      }
+    });
+  });
+
+  bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, books) => {
+      if (err) {
+        res.send(err);
+      } else {
+        return res.json(books);
+      }
+    });
+  });
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send(`Welcome To home page`);
+});
+
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
